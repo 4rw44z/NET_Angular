e7814ca5ea7e9c3ec4576d8fac4885f7b58664a8
4rw44z <arwaazness@gmail.com>
2026-04-02 01:40:03 +0530

    [ADD] JWT token baseline implementation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,18 +3,22 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
-{
+{   [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountController(AppDbContext context) : BaseApiController
+    public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
     {
         [HttpPost("register")] // api/account/register
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
-        {   if (await EmailExists(registerDto.Email)) return BadRequest("Email already exists");
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await EmailExists(registerDto.Email)) return BadRequest("Email already exists");
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
@@ -25,10 +29,37 @@ namespace API.Controllers
             };
             context.Users.Add(user);
             await context.SaveChangesAsync();
-            return user;
+            return new UserDto
+            {
+                Id = user.Id,
+                Email = user.Email,
+                DisplayName = user.DisplayName,
+                Token = tokenService.CreateToken(user),
+            };
         }
-        private async Task<bool> EmailExists(string email) {
+        private async Task<bool> EmailExists(string email)
+        {
             return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+        }
+
+        [HttpPost("login")] //api/acount/login
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+            if (user == null) return Unauthorized("Invalid email address");
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            for (var i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+            return new UserDto
+            {
+                Id = user.Id,
+                Email = user.Email,
+                DisplayName = user.DisplayName,
+                Token = tokenService.CreateToken(user),
+            };
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,14 +15,26 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("Cannot get token key");
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+        ValidateIssuer = false, // development only, should be true in production
+        ValidateAudience = false, // development only, should be true in production
+    };
+     
+});
 builder.Services.AddCors();
-
+builder.Services.AddScoped<ITokenService, TokenService>();
 var app = builder.Build();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-}
+// Middleware order matters, authentication should be before authorization
+app.UseAuthentication();
+app.UseAuthorization();
+
 
 app.UseHttpsRedirection();
 
