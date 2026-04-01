874e5379a8a763223bc16b1effe8ca2ad3bb5d2f
4rw44z <arwaazness@gmail.com>
2026-04-01 18:29:51 +0530

    [ADD] registeration api and validation

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,5 +6,9 @@ public class AppUser
     public required string DisplayName {get; set;}
     public required string Email {get; set;}
 
+    public byte[] PasswordHash {get; set;}
+
+    public byte[] PasswordSalt {get; set;}
+
 }
  
